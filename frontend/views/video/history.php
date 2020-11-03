c8edda1a68a3c772a509990785f0cc1e74472eb6
Your Name <moseoduor86@gmail.com>
2020-11-03 19:42:52 +0300

    first commit

---
+++ frontend/views/video/history.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<h4>History</h4>
+<?php echo \yii\widgets\ListView::widget([
+    'dataProvider' => $dataProvider,
+    'itemView' => '_video_item',
+    'layout' => '<div class="d-flex flex-wrap">{items}</div>{pager}',
+    'itemOptions' => [
+        'tag' => false
+    ]
+]) ?>
---
+++ frontend/views/video/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<?php echo \yii\widgets\ListView::widget([
+    'dataProvider' => $dataProvider,
+    'pager' => [
+        'class' => \yii\bootstrap4\LinkPager::class,
+    ],
+    'itemView' => '_video_item',
+    'layout' => '<div class="d-flex flex-wrap">{items}</div>{pager}',
+    'itemOptions' => [
+        'tag' => false
+    ]
+]) ?>
